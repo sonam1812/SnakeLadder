38540aea3c6af2653ff037c8ce4f412ecf78e0ba
sonam1812 <sonampal832@gmail.com>
2021-08-25 21:19:39 +0530

    Added:uc4

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -2,19 +2,37 @@ package com.SnakeLadder;
 
 public class SnakeLadder {
 	public static  void main(String[] args) {
-		 
-	        System.out.println("You got : "+roll());
-	        luck();
-	    }
+		
+		        int store = 0;
+		        while (store <= 100) {
+		            int roll = roll();
+		            System.out.println("PLayer1 got : " + roll);
+		            store += roll;
+		            System.out.println("Player1 is now at : " + store);
+		            if (store < 89 && store > 11) {
+		                switch (luck()) {
+		                    case 1 -> {
+		                        store += roll;
+		                        System.out.println("Player1 got a ladder is now at : " + store);
+		                    }
+		                    case 2 -> {
+		                        store -= roll;
+		                        System.out.println("Player1 got a snake is now at : " + store);
+		                    }
+		                }
+		            }
+		        }
+		        System.out.println("Player 1 has won");
+		    }
 
-	    public static int roll() {
-	        return (int) Math.floor(Math.random() * 6) + 1;
-	    }
+		    public static int roll() {
+		        return (int) Math.floor(Math.random() * 6) + 1;
+		    }
 
-	    public static int luck() {
-	        return (int) Math.floor(Math.random() * 3);
-	    }
-	}		      
-	
+		    public static int luck() {
+		        return (int) Math.floor(Math.random() * 3);
+		    }
+		}	 
+	       
 
 
