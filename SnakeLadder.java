994a26a3e97aa1821bd849130977d30359e605c6
sonam1812 <sonampal832@gmail.com>
2021-08-25 21:30:44 +0530

    Added:uc7

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -3,34 +3,69 @@ package com.SnakeLadder;
 public class SnakeLadder {
 	public static  void main(String[] args) {
 		
-	        int store = 0;
-	        while (store <= 99) {
-	            int roll = roll();
-	            System.out.println("PLayer1 got : " + roll);
-	            store += roll;
-	            System.out.println("Player1 is now at : " + store);
-	            if (store > 100)
-	            {
-	                store -= roll;
-	                System.out.println("Player can not surpass 100 and is reverted back to : " +store);
-	            }
-	            if (store < 89 && store > 11) {
-	                switch (luck()) {
-	                    case 1 -> {
-	                        store += roll;
-	                        System.out.println("Player1 got a ladder is now at : " + store);
-	                    }
-	                    case 2 -> {
-	                        store -= roll;
-	                        System.out.println("Player1 got a snake is now at : " + store);
-	                    }
-	                }
-	            }
-	        }
-	        System.out.println("Player 1 has won with a total dice roll of : "+count);
-	    }
+		        int store = 0;
+		        int store1 = 0;
+		        while (store <= 99 && store1 <= 99) {
+		            int roll = roll();
+		            System.out.println("PLayer1 got : " + roll);
+		            store += roll;
+		            System.out.println("Player1 is now at : " + store);
+		            if (store > 100) {
+		                store -= roll;
+		                System.out.println("Player 1 can not surpass 100 and is reverted back to : " + store);
+		            }
+		            if (store < 89 && store > 11) {
+		                switch (luck()) {
+		                    case 1 -> {
+		                        store += roll;
+		                        System.out.println("Player1 got a ladder is now at : " + store);
+		                    }
+		                    case 2 -> {
+		                        store -= 2 * roll;
+		                        System.out.println("Player1 got a snake is now at : " + store);
+		                    }
+		                }
+		            }
+		            int roll2 = roll2();
+		            System.out.println("Player2 got : " + roll2);
+		            store1 += roll2;
+		            System.out.println("Player2 is now at : " + store1);
+		            if (store1 > 100) {
+		                store1 -= roll2;
+		                System.out.println("Player 2 can not surpass 100 and is reverted back to : " + store1);
+		            }
+		            if (store1 < 89 && store1 > 11) {
+		                switch (luck()) {
+		                    case 1 -> {
+		                        store1 += roll2;
+		                        System.out.println("Player2 got a ladder is now at : " + store1);
+		                    }
+		                    case 2 -> {
+		                        store1 -= 2 * roll2;
+		                        System.out.println("Player2 got a snake is now at : " + store1);
+		                    }
+		                }
+		            }
+		        }
+		        System.out.println("Player 1 finished in : " + store);
+		        System.out.println("Player 2 finished in : " + store1);
+		        if (store > store1) {
+		            System.out.println("Player 1 has won the game. The total number of time the dice was rolled per player is : " + count);
+		        } else
+		            System.out.println("Player 2 has won the game. The total number of time the dice was rolled per player is : " + count);
+		    }
 
-	    public static int roll() {
-	        count++;
-	        return (int) Math.floor(Math.random() * 6) + 1;
-	    }	 
+		    public static int roll() {
+		        count++;
+		        return (int) Math.floor(Math.random() * 6) + 1;
+		    }
+
+		    public static int roll2() {
+		        return (int) Math.floor(Math.random() * 6) + 1;
+		    }
+
+		    public static int luck() {
+		        return (int) Math.floor(Math.random() * 3);
+		    }
+
+		 }     
