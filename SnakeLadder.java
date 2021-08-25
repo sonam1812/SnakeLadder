bbc69376f05ca5f21a210f8f209f039b9bab66b3
sonam1812 <sonampal832@gmail.com>
2021-08-25 20:59:47 +0530

    Added uc2-rollTheDice

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -2,7 +2,14 @@ package com.SnakeLadder;
 
 public class SnakeLadder {
 	public static  void main(String[] args) {
-		int store =0;
-	}
+		 
+		        System.out.println("You got : "+roll());
+		    }
 
-}
+		    public static int roll() {
+		        return (int) Math.floor(Math.random() * 6) + 1;
+		    }
+}	
+	
+
+
