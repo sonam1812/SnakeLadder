563ee9039a5d254c3c8e7b3abd314b4c00df6105
sonam1812 <sonampal832@gmail.com>
2021-08-25 21:13:04 +0530

    Added uc3

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -3,13 +3,18 @@ package com.SnakeLadder;
 public class SnakeLadder {
 	public static  void main(String[] args) {
 		 
-		        System.out.println("You got : "+roll());
-		    }
+	        System.out.println("You got : "+roll());
+	        luck();
+	    }
 
-		    public static int roll() {
-		        return (int) Math.floor(Math.random() * 6) + 1;
-		    }
-}	
+	    public static int roll() {
+	        return (int) Math.floor(Math.random() * 6) + 1;
+	    }
+
+	    public static int luck() {
+	        return (int) Math.floor(Math.random() * 3);
+	    }
+	}		      
 	
 
 
