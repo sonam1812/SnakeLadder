f58e5109baf54290cf85d820cbb70c64553ec937
sonam1812 <sonampal832@gmail.com>
2021-08-25 21:23:59 +0530

    Added:uc5

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -2,13 +2,18 @@ package com.SnakeLadder;
 
 public class SnakeLadder {
 	public static  void main(String[] args) {
-		
+		 
 		        int store = 0;
-		        while (store <= 100) {
+		        while (store <= 99) {
 		            int roll = roll();
 		            System.out.println("PLayer1 got : " + roll);
 		            store += roll;
 		            System.out.println("Player1 is now at : " + store);
+		            if (store > 100)
+		            {
+		                store -= roll;
+		                System.out.println("Player can not surpass 100 and is reverted back to : " +store);
+		            }
 		            if (store < 89 && store > 11) {
 		                switch (luck()) {
 		                    case 1 -> {
@@ -32,7 +37,5 @@ public class SnakeLadder {
 		    public static int luck() {
 		        return (int) Math.floor(Math.random() * 3);
 		    }
-		}	 
-	       
-
-
+		}	
+		        
